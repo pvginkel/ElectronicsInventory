bab1a83c45e2e354bbead387da1a4ae141ca69dd
Pieter van Ginkel <pvginkel@gmail.com>
2025-08-26 18:52:06 +0200

    Added dev command to run the dev server.

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,7 @@
 from app import create_app
 from app.config import get_settings
 
-if __name__ == "__main__":
+def main():
     settings = get_settings()
     app = create_app(settings)
 
@@ -11,3 +11,6 @@ if __name__ == "__main__":
     debug_mode = settings.FLASK_ENV in ("development", "testing")
 
     app.run(host="0.0.0.0", port=5000, debug=debug_mode)
+
+if __name__ == "__main__":
+    main()
