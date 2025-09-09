57aca5ded8689eb32293104cd0c163551db7c4ea
Pieter van Ginkel <pvginkel@gmail.com>
2025-09-09 08:57:41 +0200

    Aligned PGAdmin password with that in production.

--- run-pgadmin.sh
+++ run-pgadmin.sh
@@ -2,7 +2,7 @@
 
 docker run \
     -e PGADMIN_DEFAULT_EMAIL=pvginkel@gmail.com \
-    -e PGADMIN_DEFAULT_PASSWORD=password \
+    -e PGADMIN_DEFAULT_PASSWORD=admin \
     -p 8100:80 \
     -d \
     dpage/pgadmin4:latest
